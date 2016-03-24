ac0f792ee972716057af6afc35361fe07c1f6160
Bob Weichler <bobmasaki@hotmail.com>
2016-03-24 14:14:14 +0100

    Start homepage view (layout)

---
+++ footer.php
@@ -0,0 +1,21 @@
+	</div><!-- ./content container -->
+	
+	<footer>
+		<div class="container">
+			<ul>
+				<li><a href="#!">over ons</a></li>
+				<li><a href="#!">ondersteuning</a></li>
+				<li><a href="#!">blog</a></li>
+				<li><a href="#!">privacy</a></li>
+				<li><a href="#!">voorwaarden</a></li>
+			</ul>
+			<p class="copyright">&copy; 2016 IMDstagram</p>
+		</div><!-- ./container -->
+	</footer>
+
+	<!-- Scriptzz -->
+	<script src="assets/js/jquery-1.11.3.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
