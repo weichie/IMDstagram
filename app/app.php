dcd9ad492c2e8bbe3e3f6a48a4ec1a5e8ccfde57
Yame- <yannick@yame.be>
2016-04-05 14:48:31 +0200

    db

--- app/app.php
+++ app/app.php
@@ -5,6 +5,7 @@ spl_autoload_register(function ($class_name) {
     include 'classes/'.$class_name . '.class.php';
 });
 
+$db = new mysqli_connect('localhost', 'root', 'root', 'imdstagram');
 $app = new User;
 
 ?>
