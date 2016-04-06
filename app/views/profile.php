7edc2095f578b7e3c6babd29e3ad2c67498a9149
Bob Weichler <bobmasaki@hotmail.com>
2016-04-06 09:55:21 +0200

    push to merge, starting new branch
    new branch without htacces file

---
+++ app/views/profile.php
@@ -0,0 +1,31 @@
+<section class="acc-details">
+	<div class="col-xs-12 col-sm-4 pic">
+		<img src="https://randomuser.me/api/portraits/men/69.jpg">
+	</div><!-- ./picture -->
+	<div class="col-xs-12 col-sm-8 bio">
+		<h4>Weichiie<a href="#!">profiel bewerken</a></h4>
+		<p>
+			<strong>Bob Weichler</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit explicabo eos facere eligendi asperiores minima, nulla, ducimus eaque illo deleniti. Ratione adipisci excepturi, quidem veritatis optio fuga officiis nostrum assumenda.
+			<a href="#!">www.weichieprojects.com</a>
+		</p>
+		<ul>
+			<li><strong>31</strong> berichten</li>
+			<li><strong>98</strong> volgers</li>
+			<li><strong>93</strong> volgend</li>
+		</ul>
+	</div><!-- ./bio -->
+	<div class="clearfix"></div>
+</section><!-- ./acc-details -->
+
+<section class="acc-uploads">
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</section>
