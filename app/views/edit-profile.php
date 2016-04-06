7f1e90c1c244db7ea12fc6e6e6d6d5b2e8f2e9c9
Bob Weichler <bobmasaki@hotmail.com>
2016-04-06 14:45:04 +0200

    edit-profile pagina toegevoegd

---
+++ app/views/edit-profile.php
@@ -0,0 +1,58 @@
+<section class="acc-details">
+	<div class="col-xs-12 col-sm-4 pic">
+		<img src="https://randomuser.me/api/portraits/men/69.jpg">
+	</div><!-- ./picture -->
+	<div class="col-xs-12 col-sm-8 bio">
+		<h4>Weichiie<a href="#!">profiel bewerken</a></h4>
+		<p>
+			<strong>Bob Weichler</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit explicabo eos facere eligendi asperiores minima, nulla, ducimus eaque illo deleniti. Ratione adipisci excepturi, quidem veritatis optio fuga officiis nostrum assumenda.
+			<a href="#!">www.weichieprojects.com</a>
+		</p>
+		<ul>
+			<li><strong>31</strong> berichten</li>
+			<li><strong>98</strong> volgers</li>
+			<li><strong>93</strong> volgend</li>
+		</ul>
+	</div><!-- ./bio -->
+	<div class="clearfix"></div>
+</section><!-- ./acc-details -->
+
+<section class="acc-update">
+	<form class="form-horizontal">
+		<div class="form-group">
+			<label for="email" class="col-sm-2 control-label">Email</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="email" value="bobmasaki@hotmail.com">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="name" class="col-sm-2 control-label">Naam</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="name" value="Bob Weichler">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="username" class="col-sm-2 control-label">gebruikersnaam</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="username" value="Weichiie">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="site" class="col-sm-2 control-label">Website</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="site" value="http://www.weichieprojects.com">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="bio" class="col-sm-2 control-label">Bio</label>
+			<div class="col-sm-10">
+				<textarea type="text" rows="5" class="form-control" id="bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit explicabo eos facere eligendi asperiores minima, nulla, ducimus eaque illo deleniti. Ratione adipisci excepturi, quidem veritatis optio fuga officiis nostrum assumenda.</textarea>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-success">opslaan</button>
+			</div>
+		</div>
+	</form>
+</section>
