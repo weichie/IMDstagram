8a2b01f3c0b9e73277b9a1bec13ed28f81e4ae6a
Bob Weichler <bobmasaki@hotmail.com>
2016-04-12 08:12:37 +0200

    Login en register forms style update

--- app/views/login.php
+++ app/views/login.php
@@ -1,4 +1,4 @@
-<form action="" method="post">
+<form action="" method="post" class="login-form">
 	<div class="form-group">
 		<label for="email">Email</label>
 		<input type="email" class="form-control" name="email" id="email" placeholder="Email">
@@ -7,5 +7,7 @@
 		<label for="password">Password</label>
 		<input type="password" class="form-control" name="password" id="password" placeholder="Password">
 	</div>
-	<button type="submit" name="login" class="btn btn-default">Aanmelden</button>
+	<button type="submit" name="login" class="btn btn-primary">Aanmelden</button><br>
+	<a href="#!">Wachtwoord vergeten?</a><br>
+	<a href="?p=register">Een nieuw account registreren</a>
 </form>
--- app/views/register.php
+++ app/views/register.php
@@ -1,7 +1,7 @@
 <?php
 	
 ?>
-<form action="" method="post">
+<form action="" method="post" class="login-form">
 	<div class="form-group">
 		<label for="email">Email</label>
 		<input type="email" class="form-control" name="email" id="email" placeholder="Email">
@@ -10,5 +10,6 @@
 		<label for="password">Password</label>
 		<input type="password" class="form-control" name="password" id="password" placeholder="Password">
 	</div>
-	<button type="submit" name="register" class="btn btn-default">Registreren</button>
+	<button type="submit" name="register" class="btn btn-primary">Registreren</button><br>
+	<a href="?p=login">Aanmelden met een bestaand account</a>
 </form>
