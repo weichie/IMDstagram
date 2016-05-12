500bddecd14ca1f35a2d5f719fa12a49b3bc147c
Bob Weichler <bobmasaki@hotmail.com>
2016-05-12 16:25:17 +0200

    Post footer - like/reageren + reacties toegevoegd

--- app/views/feed.php
+++ app/views/feed.php
@@ -10,8 +10,26 @@
 	</header>
 
 	<div class="image"></div><!-- ./image -->
-
-	<footer>
-		
-	</footer>
+	
+	<div class="bottom">
+		<div class="reacties">
+			<p class="poster">
+				<strong>Weichie</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vitae fugit, ex inventore quae dolores debitis eum, perferendis iste fugiat magnam eveniet nobis reprehenderit aperiam architecto nulla officia dolor obcaecati.
+			</p>
+			<ul>
+				<li>
+					<strong>Yame</strong>
+					Lorem ipsum dolor sit amet :)
+				</li>
+				<li>
+					<strong>Yame</strong>
+					Lorem ipsum dolor sit amet :)
+				</li>
+			</ul>
+		</div><!-- ./reacties -->
+		<div class="reageren">
+			<a href="#!"><i class="fa fa-heart-o"></i></a>
+			<input type="text" class="reactie-plaatsen" placeholder="Schrijf een reactie..." />
+		</div><!-- ./reageren -->
+	</div><!-- ./bottom -->
 </section><!-- ./post -->
