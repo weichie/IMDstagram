26af2b9d944b560bce9392269364f2cbf0df349c
Yame- <yannick@yame.be>
2016-05-16 22:06:13 +0200

    Facebook login? :D

--- app/views/login.php
+++ app/views/login.php
@@ -14,6 +14,19 @@
 		<input type="password" class="form-control" name="password" id="password" placeholder="Password">
 	</div>
 	<button type="submit" name="login" class="btn btn-primary">Aanmelden</button><br>
+
+	<hr>
+
+	Of login met Facebook: 
+
+	<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+
+<br><br>
+
 	<a href="#!">Wachtwoord vergeten?</a><br>
 	<a href="<?=SITE_URL?>/?route=user/register">Een nieuw account registreren</a>
 </form>
+
+
