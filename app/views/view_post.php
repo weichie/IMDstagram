7e2080c926ca8be2b03cee24e2d27236ecda67ab
Yame- <yannick@yame.be>
2016-05-16 21:25:56 +0200

    Post UI, start geolocation

--- app/views/view_post.php
+++ app/views/view_post.php
@@ -2,15 +2,15 @@
 	<header>
 		<h5>
 			<a href="#!">
-				<img src="<?php htmlspecialchars($post['avatar'], ENT_QUOTES, 'UTF-8'); ?>" />
-				<?php htmlspecialchars($post['username'], ENT_QUOTES, 'UTF-8'); ?>
+				<img src="<?php echo htmlspecialchars($post['avatar'], ENT_QUOTES, 'UTF-8'); ?>" />
+				<?php echo htmlspecialchars($post['username'], ENT_QUOTES, 'UTF-8'); ?>
 			</a>
 			<span class="pull-right"><?=$this->ago(strtotime($post['date']))?></span>
-			<a href="<?=SITE_URL?>/?route=user/deletePost&id=<?php htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="pull-right">foto verwijderen?</a>
+			<a href="<?=SITE_URL?>/?route=user/deletePost&id=<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="pull-right">foto verwijderen?</a>
 		</h5>
 	</header>
 	
-	<figure class="<?php htmlspecialchars($post['filter'], ENT_QUOTES, 'UTF-8'); ?>">
+	<figure class="<?php echo htmlspecialchars($post['filter'], ENT_QUOTES, 'UTF-8'); ?>">
 	<div class="image" style="background-image:url('<?=$post['image_url']?>')"></div><!-- ./image -->
 	</figure>
 
@@ -20,7 +20,7 @@
 				<?php if( !empty($post['likes']) ): ?>
 						<?php foreach($post['likes'] as $like): ?>
 							<li>
-								<a href="#!"><?php htmlspecialchars($like['username'], ENT_QUOTES, 'UTF-8'); ?></strong> <?= $this->linkHashtags($like['like']); ?>
+								<a href="#!"><?php echo htmlspecialchars($like['username'], ENT_QUOTES, 'UTF-8'); ?></strong> <?= $this->linkHashtags($like['like']); ?>
 							</li>
 						<?php endforeach; ?>
 				<?php endif; ?>
@@ -36,16 +36,16 @@
 				<?php if( !empty($post['comments']) ): ?>
 						<?php foreach($post['comments'] as $comment): ?>
 							<li>
-								<strong><?php htmlspecialchars($comment['username'], ENT_QUOTES, 'UTF-8'); ?></strong> <?= $this->linkHashtags($comment['comment']); ?>
+								<strong><?php echo htmlspecialchars($comment['username'], ENT_QUOTES, 'UTF-8'); ?></strong> <?= $this->linkHashtags($comment['comment']); ?>
 							</li>
 						<?php endforeach; ?>
 				<?php endif; ?>
 			</ul>
 		</div><!-- ./reacties -->
 		<div class="reageren">
-			<a href="<?=SITE_URL?>/?route=post/like&id=<?php htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="like" data-id="<?php htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>"><i class="fa fa-heart-o"></i></a>
+			<a href="<?=SITE_URL?>/?route=post/like&id=<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="like" data-id="<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>"><i class="fa fa-heart-o"></i></a>
 			<form action="<?=SITE_URL?>/?route=post/comment" method="post">
-			<input type="hidden" value="<?php htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="post_id" name="post_id">
+			<input type="hidden" value="<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="post_id" name="post_id">
 			<input type="text" class="reactie-plaatsen" name="comment" placeholder="Schrijf een reactie..." />
 			<input type="submit" class="addComment" data-name="<?=$this->getUsername()?>" value="Post !">
 			</form>
