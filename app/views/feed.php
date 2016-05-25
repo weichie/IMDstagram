3c754f3bae1cd0b113133d5db340b46e1e890fae
Bob Weichler <bobmasaki@hotmail.com>
2016-05-25 08:58:47 +0200

    minimal style updates

--- app/views/feed.php
+++ app/views/feed.php
@@ -18,7 +18,7 @@
 		</header>
 
 		<figure class="<?=$post['filter']?>">
-		<div class="image" style="background-image:url('<?=$post['image_url']?>');"></div><!-- ./image -->
+			<div class="image" style="background-image:url('<?=$post['image_url']?>');"></div><!-- ./image -->
 		</figure>
 		
 		<div class="bottom">
@@ -26,7 +26,6 @@
 				<p class="poster">
 					<?=$this->linkHashtags($post['description'])?> <i><?php echo htmlspecialchars($post['post_location'], ENT_QUOTES, 'UTF-8');?></i>
 				</p>
-				<hr>
 				<ul class="comments_<?=$post['post_id']?>">
 					<?php if( !empty($post['comments']) ): ?>
 							<?php foreach($post['comments'] as $comment): ?>
@@ -40,9 +39,9 @@
 			<div class="reageren">
 				<a href="<?=SITE_URL?>/?route=post/like&id=<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="like" data-id="<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>"><i class="fa <?=($this->hasLiked($post['post_id'])) ? 'fa-heart' : 'fa-heart-o'?>"></i></a>
 				<form action="<?=SITE_URL?>/?route=post/comment" method="post">
-				<input type="hidden" value="<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="post_id" name="post_id">
-				<input type="text" class="reactie-plaatsen" name="comment" placeholder="Schrijf een reactie..." />
-				<input type="submit" class="addComment" data-name="<?=$this->getUsername()?>" value="Post !">
+					<input type="hidden" value="<?php echo htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8'); ?>" class="post_id" name="post_id">
+					<input type="text" class="reactie-plaatsen" name="comment" placeholder="Schrijf een reactie..." />
+					<input type="submit" class="btn btn-default btn-sm addComment pull-right" data-name="<?=$this->getUsername()?>" value="Post !">
 				</form>
 			</div><!-- ./reageren -->
 		</div><!-- ./bottom -->
--- app/views/header.php
+++ app/views/header.php
@@ -48,7 +48,6 @@
 				<div class="collapse navbar-collapse" id="menu">
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="<?=SITE_URL?>/?route=user/post"><i class="fa fa-pencil-square-o"></i></a></li>
-						<li><a href="#!"><i class="fa fa-heart"></i></a></li>
 						<li><a href="<?=SITE_URL?>/?route=user/profile"><i class="fa fa-user"></i></a></li>
 						<li><a href="<?=SITE_URL?>/?route=user/logout"><i class="fa fa-sign-out"></i></a></li>
 					</ul>
